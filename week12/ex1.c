dcc2d8b9e6b49d62da4a557eee3176553dd5352e
Dmitrii Kuzmin <1kkiren@mail.ru>
2023-11-28 23:55:09 +0300

    [Update] Comments

--- week12/ex1.c
+++ week12/ex1.c
@@ -6,6 +6,7 @@
 #include <string.h>
 #include <errno.h>
 
+// key codes
 #define E_KEY 0x12
 #define X_KEY 0x2d
 #define C_KEY 0x2e
@@ -14,6 +15,8 @@
 #define R_KEY 0x13
 #define B_KEY 0x30
 
+
+// globals for holding keys
 int e_is_holding;
 int c_is_holding;
 int a_is_holding;
@@ -21,36 +24,54 @@ int p_is_holding;
 int r_is_holding;
 
 int main(int argc, char *argv[]) {
+    // input event structure
     struct input_event ev;
     ssize_t n;
+
+    // event file and output file
     char *event_file = "/dev/input/by-path/platform-i8042-serio-0-event-kbd";
     char *output_file = "ex1.txt";
 
+    // open event file
     int fd = open(event_file, O_RDONLY);
+
+    // check if open was successful, if not exit with error
     if (fd == -1) {
         perror("open");
         exit(EXIT_FAILURE);
     }
 
+    // open output file
     FILE *out = fopen(output_file, "w");
+
+    // check if open was successful, if not exit with error
     if (out == NULL) {
         perror("fopen");
         exit(EXIT_FAILURE);
     }
 
+    // print instructions
     printf("Press E+X to exit\n");
     printf("Press P+E to get some information my exam\n");
     printf("Press C+A+P to get some cappuccino\n");
     printf("Press R+A to get some information about Ra\n");
 
+    // save instructions to file
     fprintf(out, "Press E+X to exit\n");
     fprintf(out, "Press P+E to get some information my exam\n");
     fprintf(out, "Press C+A+P to get some cappuccino\n");
     fprintf(out, "Press R+A to get some information about Ra\n");
 
     while (1) {
+        // read event
         n = read(fd, &ev, sizeof ev);
 
+        /*
+        If n is -1, it checks if the error is EINTR (interrupted system call),
+        in which case it continues the loop. For any other error, or if n is
+        not equal to the size of ev, it sets errno to EIO (input/output error)
+        and breaks the loop.
+        */
         if (n == (ssize_t) -1) {
             if (errno == EINTR)
                 continue;
@@ -61,7 +82,7 @@ int main(int argc, char *argv[]) {
             break;
         }
 
-
+        // if event is a key event and the value is 0, 1 or 2
         if (ev.type == EV_KEY && ev.value >= 0 && ev.value <= 2) {
             if (ev.value == 0) {
                 fprintf(out, "RELEASED 0x%04x (%d)\n", ev.code, ev.code);
@@ -75,10 +96,11 @@ int main(int argc, char *argv[]) {
             }
         }
 
-
+        // check if E is holding
         if (ev.code == E_KEY && (ev.value == 1 || ev.value == 2)) {
             e_is_holding = 1;
         } else if (ev.code == X_KEY && (ev.value == 1 || ev.value == 2) && e_is_holding == 1) {
+            // if E is holding and X is pressed, exit
             printf("EXIT\n");
             fprintf(out, "EXIT\n");
             break;
@@ -86,10 +108,13 @@ int main(int argc, char *argv[]) {
             e_is_holding = 0;
         }
 
+        // check if C is holding
         if (ev.code == C_KEY && (ev.value == 1 || ev.value == 2)) {
             c_is_holding = 1;
         } else if (ev.code == A_KEY && (ev.value == 1 || ev.value == 2) && c_is_holding == 1) {
+            // if C is holding and A is pressed, then check if P is holding
             if (p_is_holding == 1) {
+                // if P is holding, print the cappuccino message and reset P
                 printf("Get some cappuccino!\n");
                 fprintf(out, "Get some cappuccino!\n");
                 p_is_holding = 0;
@@ -100,18 +125,22 @@ int main(int argc, char *argv[]) {
             c_is_holding = 0;
         }
 
+        // check if P is holding
         if (ev.code == P_KEY && (ev.value == 1 || ev.value == 2)) {
             p_is_holding = 1;
         } else if (ev.code == E_KEY && (ev.value == 1 || ev.value == 2) && p_is_holding == 1) {
+            // if P is holding and E is pressed, print message
             printf("I passed the Exam!\n");
             fprintf(out, "I passed the Exam!\n");
         } else if (ev.code == P_KEY && ev.value == 0) {
             p_is_holding = 0;
         }        
 
+        // check if R is holding
         if (ev.code == R_KEY && (ev.value == 1 || ev.value == 2)) {
             r_is_holding = 1;
         } else if (ev.code == A_KEY && (ev.value == 1 || ev.value == 2) && r_is_holding == 1) {
+            // if R is holding and A is pressed, print the info about Ra
             printf("Ra (also given as Re) is the sun god of ancient Egypt. He is one of the oldest deities in the Egyptian pantheon and was later merged with others such as Horus, becoming Ra-Horakhty (the morning sun), Amun (as noonday sun), and Atum (the evening sun) associated with primal life-giving energy.\n");
             fprintf(out, "Ra (also given as Re) is the sun god of ancient Egypt. He is one of the oldest deities in the Egyptian pantheon and was later merged with others such as Horus, becoming Ra-Horakhty (the morning sun), Amun (as noonday sun), and Atum (the evening sun) associated with primal life-giving energy.\n");
         } else if (ev.code == R_KEY && ev.value == 0) {
@@ -120,6 +149,7 @@ int main(int argc, char *argv[]) {
 
     }
 
+    // close files
     fclose(out);
     close(fd);
 
